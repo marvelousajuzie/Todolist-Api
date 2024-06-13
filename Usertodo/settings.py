d0f21654bf675f3ae29fe2db0e1372f6ce9b5717
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-06-13 02:00:53 +0100

    hide secretkey

--- Usertodo/settings.py
+++ Usertodo/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c0$l-#rce=%c!jaanbq75wwi!gg=b*lyamn9$#(m4ktehgt7rz'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS =  ['todolist-api-cv4n.onrender.com']
-
+# ALLOWED_HOSTS =  []
 
 # Application definition
 
